f3d6f5351606c7b635608ee6cd4f01ab7d90bc2c
Lucas Tavano <lucas.c.tavano@gmail.com>
2026-04-17 09:09:49 -0300

    fix: exclude /healthz from force_ssl redirect

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :o_grupo_de_estudos, OGrupoDeEstudosWeb.Endpoint,
   force_ssl: [
     rewrite_on: [:x_forwarded_proto],
     exclude: [
-      # paths: ["/health"],
+      paths: ["/healthz"],
       hosts: ["localhost", "127.0.0.1"]
     ]
   ]
